05f3e50a8e6b899c8d8fbe7c0bede9a24158ba3c
tawaresachin <sachintaware.2551988@gmail.com>
2021-03-13 20:40:51 +0530

    13march

---
+++ java-practice/src/JavaBasics/BaseClass.java
@@ -0,0 +1,21 @@
+package JavaBasics;
+
+public class BaseClass {
+	
+	/* Access Specifier
+	 * 1. Public - throughout the project
+	 * 2. Default- within a specific package
+	 * 3. Private- only within class
+	 * 4. Protected - within the package but inherited
+	 
+	 Access Modifier-
+	 *1. Static- belongs to class (uses memory of class)
+	 *2. Final- it remains unchanged over the classes
+	 */
+	
+	public static void main(String[] args) 
+	{
+		System.out.println("Test");
+	}
+
+}
---
+++ java-practice/src/JavaBasics/test1.java
@@ -0,0 +1,31 @@
+package JavaBasics;
+
+public class test1 
+{
+	int i=10;
+	double j=20;
+	String s="Testing Class";
+	
+	/* Datatypes-
+	 A.Primitive datatypes (m/m size is fixed)
+	 * 1. byte- 8-bit/1-byte
+	 * 2. short- 2 bytes
+	 * 3. int- 4 bytes
+	 * 4. long- 8 bytes
+	 * 5. float- 4 bytes
+	 * 6. double- 8 bytes
+	 * 7. char - 2 bytes
+	 * 8. boolean - 1 bit
+	 
+	 B. Non Primitive datatypes (m/m size is adaptive)
+	  *1. Class
+	  *2. String
+	 */
+	
+	public void method()
+	{
+		System.out.println("Batch1");
+		System.out.println(s);
+	}
+
+}
